a59a143a4a6e7f88f07043d2318cc53204a781ce
zzelda2025 <vinh.thai@vfs.com.vn>
2026-04-02 22:25:28 +0700

    Update variables.tf

--- proxmox/variables.tf
+++ proxmox/variables.tf
@@ -78,3 +78,25 @@ variable "vm_password" {
   type        = string
   sensitive   = true
 }
+
+
+# ==============================================================
+# Variables cho cấu hình tích hợp AWX
+# Các giá trị này sẽ được điền từ Workspace Variables trên HCP
+# ==============================================================
+
+variable "awx_url" { 
+  description = "Địa chỉ URL của máy chủ AWX (VD: https://awx.vinhthai.io.vn)"
+  type        = string 
+}
+
+variable "awx_job_id" { 
+  description = "ID của Workflow Job Template trên AWX"
+  type        = string 
+}
+
+variable "awx_token" { 
+  description = "Access Token của user AWX có quyền chạy Job"
+  type        = string
+  sensitive   = true 
+}
