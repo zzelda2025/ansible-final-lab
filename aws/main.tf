f69ccd5cc9d29bc556954559c49d0a407bd98bd6
zzelda2025 <vinh.thai@vfs.com.vn>
2026-04-03 01:07:59 +0700

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -27,3 +27,17 @@ output "alb_dns_name" {
   description = "DNS của ALB"
   value       = module.aws_web.alb_dns_name
 }
+
+resource "null_resource" "trigger_awx" {
+  # Đảm bảo chỉ chạy sau khi EC2 đã có IP thành công
+  depends_on = [module.aws_web]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      curl -X POST "${var.awx_url}/api/v2/workflow_job_templates/${var.awx_workflow_id}/launch/" \
+           -H "Content-Type: application/json" \
+           -H "Authorization: Bearer ${var.awx_token}" \
+           -d '{"extra_vars": {"target_ips": "${module.aws_web.ec2_public_ip}"}}'
+    EOT
+  }
+}
